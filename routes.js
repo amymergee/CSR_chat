f231e4a6b922156d48c91d891f76cfe4e2c98720
Amy Rule <arule@youthlightbooks.com>
2017-01-05 10:31:44 -0500

    Changed to open a specific room instead of random

--- routes.js
+++ routes.js
@@ -20,7 +20,9 @@ module.exports = function(app,io){
 	app.get('/create', function(req,res){
 
 		// Generate unique id for the room
-		var id = Math.round((Math.random() * 1000000));
+		// var id = Math.round((Math.random() * 1000000));
+		// Open a specific room
+		var id = 100;
 
 		// Redirect to the random room
 		res.redirect('/chat/'+id);
